a39152dbbbbaa6fd7910e5a63b202e1b37849e67
Alejandro Valera <ale24vv@gmail.com>
2025-02-26 20:37:04 -0400

    first commit

---
+++ src/js/langs.js
@@ -0,0 +1,61 @@
+
+const langs = {
+    
+    en: {
+        home: {
+            title: 'Home'
+        },
+        about: {
+            title: "About Me",
+            text: "I am a front-end developer in training, passionate about creating modern and responsive interfaces. Through intensive courses, I have mastered languages like JavaScript and TypeScript and technologies like Tailwind, and I am ready to apply them in real projects."
+        },
+        skills: {
+            title: "Skills"
+        },
+        journey: {
+            title: "My Journey",
+            text: "I completed courses on platforms like Udemy where I learned to build web applications from scratch. I mastered creating responsive websites, languages like PHP and JavaScript, and building databases with MySQL and connecting APIs. Currently, I am exploring React and Vue.js to expand my skills."
+        },
+        projects: {
+            title: "Projects",
+            github: "Github Repositories",
+            netlify: "Netlify Deployments"
+        },
+        contact: {
+            title: "Contact"
+        },
+        footer: {
+            cv: "Download my CV (PDF)",
+        }
+    },
+    
+    es: {
+        home: {
+            title: 'Inicio'
+        },
+        about: {
+            title: "Sobre Mí",
+            text: "Soy un desarrollador front-end en formación, apasionado por crear interfaces modernas y responsivas. A través de cursos intensivos, he dominado idiomas como JavaScript y TypeScript y tecnologías como Tailwind, y estoy listo para aplicarlas en proyectos reales."
+        },
+        skills: {
+            title: "Habilidades"
+        },
+        journey: {
+            title: "Mi Camino",
+            text: "Completé cursos en plataformas como Udemy donde aprendí a construir aplicaciones web desde cero. Dominé la creación de sitios web responsivos, lenguajes como PHP y JavaScript, y la construcción de bases de datos con MySQL y la conexión de APIs. Actualmente, estoy explorando React y Vue.js para ampliar mis habilidades."
+        },
+        projects: {
+            title: "Proyectos",
+            github: "Ver Repositorios",
+            netlify: "Ver Más Proyectos en Deployment"
+        },
+        contact: {
+            title: "Contacto"
+        },
+        footer: {
+            cv: "Descarga mi Currículum (PDF)",
+        }
+    }
+};
+
+export default langs;
---
+++ src/js/projects.js
@@ -0,0 +1,55 @@
+
+const proyectos = [
+    
+    { 
+        nombre: "APV", 
+        imagen: "assets/projects/proyecto1.png",
+        url: "https://admin-pacientes-veterinario.netlify.app", 
+        categoria: "Frontend/Backend" 
+    },
+    { 
+        nombre: "Techno EDM", 
+        imagen: "assets/projects/proyecto2.png", 
+        url: "https://music-festival-mx.netlify.app", 
+        categoria: "Frontend" 
+    },
+    { 
+        nombre: "Nucleus Wallet",
+        imagen: "assets/projects/proyecto3.png",
+        url: "https://nucleus-wallet-website.netlify.app",
+        categoria: "Frontend" 
+    },    
+    { 
+        nombre: "Tech Pro",
+        imagen: "assets/projects/proyecto4.png",
+        url: "https://techpro-website.netlify.app",
+        categoria: "Frontend" 
+    },
+    { 
+        nombre: "Blog de Café",
+        imagen: "assets/projects/proyecto5.png",
+        url: "https://blog-de-cafe-blog.netlify.app",
+        categoria: "Frontend" 
+    },
+    { 
+        nombre: "Delivery App",
+        imagen: "assets/projects/proyecto6.png",
+        url: "https://web-delivery-app.netlify.app",
+        categoria: "Frontend" 
+    },
+    { 
+        nombre: "Web AB",
+        imagen: "assets/projects/proyecto7.png",
+        url: "https://arquitectura-site-index.netlify.app",
+        categoria: "Frontend" 
+    },
+    { 
+        nombre: "Podcast App",
+        imagen: "assets/projects/proyecto8.png",
+        url: "https://podcast-app-website.netlify.app",
+        categoria: "Frontend" 
+    },
+];
+
+
+export default proyectos;
